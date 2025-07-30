78e97d4d8c0d54b315f9b39af9f665f3884faead
wairagu <wairagufrancis02@gmail.com>
2025-07-30 15:14:28 +0300

    Initial commit

---
+++ day32.py
@@ -0,0 +1,7 @@
+import random
+
+greetings = ["Hello there!", "Jambo!", "Sasa!", "Konnichiwa!", "Guten tag!", "Bore Da!", "Hola!", "Bonjuor!"]
+
+#print(len(greetings))
+rand = random.randint(0,7)
+print(greetings[rand])
